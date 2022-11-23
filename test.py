261456744fbd337d07c911f6154131bdb90dbd7d
Joshua Ekollo <78730734+joshua87000@users.noreply.github.com>
2022-11-23 01:50:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,12 @@ while True:
     cut = frame
     cut = frame[50 : 300 , 50 : 350]
 
+    rows, cols, _  = cut.shape
+
     gris = cv2.cvtColor(cut ,cv2.COLOR_BGR2GRAY)
     gris = cv2.GaussianBlur(gris , (7,7),0)
     _, threshold = cv2.threshold(gris ,seuil  , 255 , cv2.THRESH_BINARY_INV);
-    _, threshold2 = cv2.threshold(gris , 140 , 230 , cv2.THRESH_BINARY);
+    _, threshold2 = cv2.threshold(gris , 160 , 255 , cv2.THRESH_BINARY);
 
     contours,hierachy =  cv2.findContours(threshold , cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours , key = lambda x: cv2.contourArea(x), reverse=True)
@@ -24,6 +26,9 @@ while True:
         (x,y,w,h) = cv2.boundingRect(cnt)
         #cv2.drawContours(cut,[cnt], -1 , (0 , 0 , 255) , 2)
         cv2.rectangle(cut , (x,y),(x+w, y+h),(255,0,0),2)
+
+        cv2.line(cut,(x + int(w/2),0) , (x + int(w/2), rows),(0,255,0),2)
+        cv2.line(cut,(0,y + int(h/2)) , (cols,y + int(h/2) ),(0,255,0),2)
         print(w)
 
         break
@@ -33,7 +38,7 @@ while True:
     
 
     cv2.imshow("Filtre 1" , threshold)
-    #cv2.imshow("Filtre 2" , threshold2)
+    cv2.imshow("Filtre 2" , threshold2)
     cv2.imshow("frame" , cut)
     key = cv2.waitKey(30)
     if key == 27:
